0f74e53eaa6928de6db9c1a59d7017c28c6699f1
Murugan411 <murugan.a0411@gmail.com>
2025-12-11 16:13:16 +0530

    Add files via upload

---
+++ Java_training/swap.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class swap {
+    public static void main(String[]args){
+        int a=15;
+        int b=24;
+        int temp=34;
+        System.out.println(a);
+        System.out.println(temp);
+
+
+    }
+}
