5c165211effb61c65c0785f56e61d73777932fec
Murugan411 <murugan.a0411@gmail.com>
2025-12-13 14:15:42 +0530

    Add files via upload

---
+++ conditinal_java/con_switchbased_java/sc5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class sc5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        switch (a) {
+            case 1:
+            {
+                System.err.println("withdraw");
+                break; 
+            }
+            case 2:
+            {
+                System.out.println("deposit");
+                break;
+            }
+            case 3:
+            {
+                System.out.println("check balance");
+                break;
+            }
+            case 4:
+            {
+                System.out.println("Exit");
+            }
+            default:
+                System.out.println("NO option");   
+        }
+        sc.close();
+    }
+
+}
+
